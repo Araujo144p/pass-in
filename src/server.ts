b5d99c690938e208eba908797a4cebcc46969449
araujo144p <kircaua5661@gmail.com>
2024-04-14 13:02:41 -0300

    create prisma SQLite

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Teste-1')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', (request, reply)=>{
+    return 'Hello ignite'
+})
+
+app.listen({port: 3030}).then(() =>{
+    console.log('server is running!')
+})
