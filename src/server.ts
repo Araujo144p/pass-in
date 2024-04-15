4630dbb8cd70f0aa20a0eb6fb94ca53a0b921388
araujo144p <kircaua5661@gmail.com>
2024-04-15 18:08:05 -0300

    create event post

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
+import { serializerCompiler, validatorCompiler,} from 'fastify-type-provider-zod'
+import { createEvent } from './routes/create-event'
 
 const app = fastify()
 
-app.get('/', (request, reply)=>{
-    return 'Hello ignite'
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register( createEvent )
 
 app.listen({port: 3030}).then(() =>{
     console.log('server is running!')
