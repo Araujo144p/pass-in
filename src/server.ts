d0e4624ed551a56da0ebb214047d557a9eb1ee82
araujo144p <kircaua5661@gmail.com>
2024-04-20 19:39:48 -0300

    check-in-route-created

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
 import { getEvent } from './routes/get-events'
 import { getAttendeesBadge } from './routes/get-attendees-badge'
+import { checkIn } from './routes/check-in'
 
 
 const app = fastify()
@@ -15,6 +16,7 @@ app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeesBadge)
+app.register(checkIn)
 
 app.listen({port: 3030}).then(() =>{
     console.log('server is running!')
