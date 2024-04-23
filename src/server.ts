03fc3d55fdb8cfb062fa4af7d1d2f7064e571604
araujo144p <kircaua5661@gmail.com>
2024-04-22 21:11:36 -0300

    docs-with-fastify-swagger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
-import { serializerCompiler, validatorCompiler,} from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform} from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
 import { getEvent } from './routes/get-events'
@@ -9,6 +11,23 @@ import { getEventAttendee } from './routes/get-event-attendee'
 
 
 const app = fastify()
+
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        info: {
+            title: 'pass.in',
+            description: 'Especificações da API para o back-end da aplicação pass.in',
+            version: '1.0.0'
+        },
+    },
+    transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUI, {
+    routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
