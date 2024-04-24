141020d7db731907a35a05a94f4ee7b020aa0b97
araujo144p <kircaua5661@gmail.com>
2024-04-23 21:00:00 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
+import fastifyCors from '@fastify/cors'
 import { serializerCompiler, validatorCompiler, jsonSchemaTransform} from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
@@ -12,6 +13,10 @@ import { errorHandler } from './error-handler'
 
 
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(fastifySwagger, {
     swagger: {
@@ -42,6 +47,6 @@ app.register(getEventAttendee)
 
 app.setErrorHandler(errorHandler)
 
-app.listen({port: 3030}).then(() =>{
+app.listen({ port: 3030, host: '0.0.0.0' }).then(() =>{
     console.log('server is running!')
 })
